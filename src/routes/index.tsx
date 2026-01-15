d0f5e67502cd1ad87059194961b805b9600cf06d
PiotrCembrowski <losttape@gmail.com>
2026-01-15 20:29:31 +0100

    tailiwnd

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Header } from "@/components/header";
-import { Hero } from "@/components/hero";
-import { StatsSection } from "@/components/stats-section";
-import { FeaturesSection } from "@/components/features-section";
-import { ValueProps } from "@/components/value-props";
-import { CTASection } from "@/components/cta-section";
-import { Footer } from "@/components/footer";
+import { Header } from "@/components/Header/Header";
+import { Hero } from "@/components/Home/hero";
+import { StatsSection } from "@/components/Home/stats-section";
+import { FeaturesSection } from "@/components/Home/feature-section";
+import { ValueProps } from "@/components/Home/value-props";
+import { CTASection } from "@/components/Home/cta-section";
+import { Footer } from "@/components/Footer/Footer";
 import "../App.css";
 
 export const Route = createFileRoute("/")({ component: App });
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'url'
-import { nitro } from 'nitro/vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from "url";
+import { nitro } from "nitro/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   plugins: [
@@ -17,12 +19,16 @@ const config = defineConfig({
     nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
 
     tanstackStart(),
     viteReact(),
+    tsConfigPaths(),
+    tanstackStart(),
+    viteReact(),
+    tailwindcss(),
   ],
-})
+});
 
-export default config
+export default config;
